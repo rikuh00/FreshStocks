81b0c097c6219b592112753a38f52c7963361f79
Shahzeb-Ahmed <m.shahzeb.ahmed51@gmail.com>
2020-08-22 11:29:35 -0600

    Stock Suggester (Incomplete)

--- stock_suggestion.py
+++ stock_suggestion.py
@@ -1,7 +1,15 @@
-dic = {"Music" : ["Spotify", "Tencent Music", "Vivendi", "Yamaha Corporation", "QRS Music Technologies"],
-       "Fashion" : ["H&M", "Gap", "Nike", "Guess", "Adidas"],
-       "Entertainment" : ["The Walt Disney Company", "Netflix", "Six Flags", "21st Century Fox", "Cineplex"],
-       "Sports" : ["Under Armour", "Puma", "Dick's Sporting Goods", "Dover Motorsports", "Boyd Gaming"],
-       "Technology" : ["Alphabet Inc", "Tesla", "Amazon", "Apple", "Microsoft"]
+from Asset import Asset
+
+dic = {"Music" : [["Spotify", "SPOT"], ["Tencent Music", "TME"], ["Vivendi", "VIV"], ["Warner Music Group", "WMG"], ["QRS Music Technologies", "QRSM"]],
+       "Fashion" : [["Pandoras", "PNDORA"], ["Gap", "GPS"], ["Nike", "NKE"], ["Guess", "GES"], ["Adidas", "ADDYY"]],
+       "Entertainment" : [["The Walt Disney Company", "DIS"], ["Netflix", "NFLX"], ["Six Flags", "SIX"], ["21st Century Fox", "FOX"], ["Cineplex", "CGX.TO"]],
+       "Sports" : [["Under Armour", "UA"], ["Puma", "PUM"], ["Dick's Sporting Goods", "DKS"], ["Dover Motorsports", "DVD"], ["Boyd Gaming", "BYD"]],
+       "Technology" : [["Alphabet Inc", "GOOGL"], ["Tesla", "TSLA"], ["Amazon", "AMZN"], ["Apple", "AAPL"], ["Microsoft", "MSFT"]]
        }
 
+
+for x in dic:
+       print(x)
+       for i in range(len(dic[x])):
+              print(i)
+              dic[x][i] = [Asset(dic[x][i][0], dic[x][i][1])]
