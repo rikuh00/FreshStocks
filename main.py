9923b72c6b2da3c1dddc41d29367e78caf6d5bc6
AmmolS <ammols2000@gmail.com>
2020-08-22 12:23:46 -0400

    Added more view functions for varying stock categories

--- main.py
+++ main.py
@@ -14,4 +14,47 @@ def index():
     asset_list = execute_order(initial_cash, asset_list)
     return render_template('index.html', title='Home', asset_list=asset_list)
 
+@app.route('/Music')
+def music_page():
+    initial_cash = 500
+    '''
+    Loop through the music stocks here and display them in the html
+    '''
+    asset_list = [['Spotify', 'SPOT', 2], ['Snapchat', 'SNAP', 0], ['Lululemon', 'Lulu', 3], ['Fitbit', 'FIT', 2]]
+    asset_list = execute_order(initial_cash, asset_list)
+    return render_template('index.html', title='Home', asset_list=asset_list)
+
+
+@app.route('/Fashion')
+def fashion_page():
+    initial_cash = 500
+    asset_list = [['Spotify', 'SPOT', 2], ['Snapchat', 'SNAP', 0], ['Lululemon', 'Lulu', 3], ['Fitbit', 'FIT', 2]]
+    asset_list = execute_order(initial_cash, asset_list)
+    return render_template('index.html', title='Home', asset_list=asset_list)
+
+
+@app.route('/Entertainment')
+def entertainment_page():
+    initial_cash = 500
+    asset_list = [['Spotify', 'SPOT', 2], ['Snapchat', 'SNAP', 0], ['Lululemon', 'Lulu', 3], ['Fitbit', 'FIT', 2]]
+    asset_list = execute_order(initial_cash, asset_list)
+    return render_template('index.html', title='Home', asset_list=asset_list)
+
+
+@app.route('/Sports')
+def sports_page():
+    initial_cash = 500
+    asset_list = [['Spotify', 'SPOT', 2], ['Snapchat', 'SNAP', 0], ['Lululemon', 'Lulu', 3], ['Fitbit', 'FIT', 2]]
+    asset_list = execute_order(initial_cash, asset_list)
+    return render_template('index.html', title='Home', asset_list=asset_list)
+
+
+@app.route('/Technology')
+def technology_page():
+    initial_cash = 500
+    asset_list = [['Spotify', 'SPOT', 2], ['Snapchat', 'SNAP', 0], ['Lululemon', 'Lulu', 3], ['Fitbit', 'FIT', 2]]
+    asset_list = execute_order(initial_cash, asset_list)
+    return render_template('index.html', title='Home', asset_list=asset_list)
+
 app.run(debug=True)
+
