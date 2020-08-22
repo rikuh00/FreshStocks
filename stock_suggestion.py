b6e2678fcade2dc9d4195cb5b4ebc4a3c352068d
Shahzeb-Ahmed <m.shahzeb.ahmed51@gmail.com>
2020-08-22 10:06:35 -0600

    Stock Suggester (Incomplete)

---
+++ stock_suggestion.py
@@ -0,0 +1,7 @@
+dic = {"Music" : ["Spotify", "Tencent Music", "Vivendi", "Yamaha Corporation", "QRS Music Technologies"],
+       "Fashion" : ["H&M", "Gap", "Nike", "Guess", "Adidas"],
+       "Entertainment" : ["The Walt Disney Company", "Netflix", "Six Flags", "21st Century Fox", "Cineplex"],
+       "Sports" : ["Under Armour", "Puma", "Dick's Sporting Goods", "Dover Motorsports", "Boyd Gaming"],
+       "Technology" : ["Alphabet Inc", "Tesla", "Amazon", "Apple", "Microsoft"]
+       }
+
