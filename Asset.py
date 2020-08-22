c367d79d6b5bb62a0bb702f91577f0137a050656
rikuh00 <45102134+rikuh00@users.noreply.github.com>
2020-08-21 23:13:48 -0400

    Added comments

--- Asset.py
+++ Asset.py
@@ -6,20 +6,20 @@ class Asset():
     long_avg = 200
     window = 365
     def __init__(self, name, ticker, start_date=dt.today()-timedelta(long_avg + window), end_date=dt.today()):
-        self.price = load_data(ticker, start_date, end_date)
-        self.name = name
+        self.price = load_data(ticker, start_date, end_date) #data on the price (high, low, and close)
+        self.name = name #name of the stock (e.g. "Apple")
 
     def set_bollinger(self):
-        bollinger_avg = 20
-        std_limit = 2
+        bollinger_avg = 20 #number of days for the moving average
+        std_limit = 2 #upper and lower limit for standard dev
         self.bollinger = self.price[['close']].copy()
-        self.bollinger['tp'] = self.price[['high','low','close']].mean(axis=1)
-        self.bollinger['ma'] = self.bollinger.tp.rolling('{}D'.format(bollinger_avg)).mean()
-        std = self.bollinger.ma.std()
-        self.bollinger['u_lim'] = self.bollinger.ma + (std * std_limit)
+        self.bollinger['tp'] = self.price[['high','low','close']].mean(axis=1) #typical price (see Investopedia)
+        self.bollinger['ma'] = self.bollinger.tp.rolling('{}D'.format(bollinger_avg)).mean() #moving average
+        std = self.bollinger.ma.std() #standard dev on MA
+        self.bollinger['u_lim'] = self.bollinger.ma + (std * std_limit) #upper and lower limit
         self.bollinger['l_lim'] = self.bollinger.ma - (std * std_limit)
 
     def set_ma(self):
         self.ma = self.price[['close']].copy()
-        self.ma['short_ma'] = self.ma[['close']].rolling('{}D'.format(self.short_avg)).mean()
-        self.ma['long_ma'] = self.ma[['close']].rolling('{}D'.format(self.long_avg)).mean()
+        self.ma['short_ma'] = self.ma[['close']].rolling('{}D'.format(self.short_avg)).mean() #short MA
+        self.ma['long_ma'] = self.ma[['close']].rolling('{}D'.format(self.long_avg)).mean() #long MA
